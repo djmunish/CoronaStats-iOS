f0463da983c4745c36f430b8296f690ab22d9404
Munish Sehdev <dev.munishsehdev@gmail.com>
2020-07-25 22:08:28 -0400

    minor fix

--- CoronaTracker/Views/CountrySelector.swift
+++ CoronaTracker/Views/CountrySelector.swift
@@ -69,7 +69,7 @@ struct CountrySelector: View {
                 
                 let id = NSLocale.localeIdentifier(fromComponents: [NSLocale.Key.countryCode.rawValue: code])
                 let name = NSLocale(localeIdentifier: "en_UK").displayName(forKey: NSLocale.Key.identifier, value: id) ?? "Country not found for code: \(code)"
-
+                print(name)
                 if let flagImage = UIImage(named: name.lowercased().replacingOccurrences(of: " ", with: "-")) {
                     countries.append(Country(name: name, code: code, flagImage: flagImage))
                 }
